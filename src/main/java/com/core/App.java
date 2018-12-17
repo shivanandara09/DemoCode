3888ba88ebeb4e80b2775b2e77067c8175c030cb
shivananda Rai <shivanandarai09@gmail.com>
2018-12-17 20:07:36 +0530

    first commit

---
+++ src/main/java/com/core/App.java
@@ -0,0 +1,16 @@
+package com.core;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+    public static void disp()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/GitHub/SampleDemo.java
@@ -0,0 +1,12 @@
+package com.GitHub;
+
+import org.testng.annotations.Test;
+
+public class SampleDemo {
+
+	@Test
+	public void demo()
+	{
+		System.out.println("hi demo");
+	}
+}
